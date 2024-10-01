13871326d98d54b60e602bfc7275d4b4790a61cf
saadthenexus <hasibulkarimsaad@gmail.com>
2024-10-01 11:25:00 +0600

    Tried to implement open/close principle

---
+++ Book.java
@@ -0,0 +1,16 @@
+class Book {
+    private String title;
+    private String author;
+    private String genre;
+    private int pages;
+    private String dateRead;
+
+    public Book(String title, String author, String genre, int pages, String dateRead) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.pages = pages;
+        this.dateRead = dateRead;
+    }
+
+}
